8d588dca0933bc647f16eb9ee4f928378d294af4
callosaurus <callumdavies421@gmail.com>
2017-02-16 23:01:43 -0800

    first commit

---
+++ iOS Gestures/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS Gestures
+//
+//  Created by Callum Davies on 2017-02-16.
+//  Copyright © 2017 Callum Davies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
